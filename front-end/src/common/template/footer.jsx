5f33377c5b550fcaec76130c61297bb40cfd9fea
EmersonCDias <emerson_cdias@hotmail.com>
2019-01-12 16:01:42 -0200

    Front-end started

---
+++ front-end/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+  <footer>
+    <div className="main-footer">
+      <strong>
+        Copyright &copy; 2019
+      </strong>
+      <a href="https://www.facebook.com/emerson.carvalhodias">Emerson Carvalho dias</a>
+    </div>
+  </footer>
+)
---
+++ front-end/src/common/template/menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MenuItem from './menu-item'
+import MenuTree from './menu-tree'
+
+export default props => (
+  <ul className="sidebar-menu">]
+    <MenuItem path="#" label="Dashboard" icon="dashboard" />
+    <MenuTree label="Cadastro" icon="edit">
+      <MenuItem
+        path="#billingCycles"
+        label="Ciclo de pagamentos"
+        icon="usd"
+      />
+    </MenuTree>
+  </ul>
+)
